37a76a2ede1ab705372eda4fb094870c5a9da934
ItsOnlyCole <itsonlycole@gmail.com>
2021-02-16 13:49:48 -0500

    Clean Code

--- t2tt.py
+++ t2tt.py
@@ -8,25 +8,34 @@ clipName = clipFile[0:-4]
 def blur(image):
     return cv2.GaussianBlur(image, (99,99), cv2.BORDER_DEFAULT)
 
+#Creates a blank color clip that is used to set the resolution
+def createResolutionClip():
+    return ColorClip((1080,1920), (0,0,0), duration=1)
 
-#Creates a blank color clip that's used to set the resolution
-resolutionClip = ColorClip((1080,1920), (0,0,0), duration=1)
-#Background clip is set to fill screen and blurred
-backgroundClip = VideoFileClip(clipFile).fx( vfx.resize, width=3413)
-backgroundClip = backgroundClip.fl_image(blur)
-backgroundClip = backgroundClip.set_position('center')
-#Center Focus clip
-focusClip = VideoFileClip(clipFile).fx( vfx.resize, width=1080 )
-focusClip = focusClip.set_position('center')
+#Setup background clip
+def createBackgroundClip(originalClip):
+    clip = VideoFileClip(originalClip).fx(vfx.resize, width=3413)
+    clip = clip.fl_image(blur)
+    clip = clip.set_position('center')
+    return clip
+#Sets up focus clip
+def createFocusClip(originalClip):
+    clip = VideoFileClip(originalClip).fx(vfx.resize, width=1080)
+    clip = clip.set_position('center')
+    return clip
 
 
-#Combines clips and renders
-### Clips stack bottom to top
-final_clip = CompositeVideoClip([resolutionClip, backgroundClip, focusClip])
-final_clip.write_videofile(clipName+"TikTok.mp4")
+if __name__ == '__main__':
+    resolutionClip = createResolutionClip()
+    backgroundClip = createBackgroundClip(clipFile)
+    focusClip = createFocusClip(clipFile)
 
+    #Combines clips and renders
+    ### Clips stack back to front
+    final_clip = CompositeVideoClip([resolutionClip, backgroundClip, focusClip])
+    final_clip.write_videofile(clipName+"TikTok.mp4")
 
-#Closing clips for cleanup
-resolutionClip.close()
-backgroundClip.close()
-focusClip.close()
+    #Closing clips for cleanup
+    resolutionClip.close()
+    backgroundClip.close()
+    focusClip.close()
