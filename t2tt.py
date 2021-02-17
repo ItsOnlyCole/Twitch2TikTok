04e723915ac0fafdddbb710378c02c1945ecd2a7
ItsOnlyCole <itsonlycole@gmail.com>
2021-02-17 13:17:42 -0500

    Add new arguments to argparse

--- t2tt.py
+++ t2tt.py
@@ -1,6 +1,14 @@
 from moviepy.editor import *
 import sys
 import cv2
+import argparse
+
+parser = argparse.ArgumentParser(description="Automate editing gameplay clips for Tik Tok")
+parser.add_argument('-f', '--file', metavar='', required=True, help="file to edit and render")
+parser.add_argument('-Sc', '--Start', metavar='', require=False, help="custom start time of clip")
+parser.add_argument('-Ec', '--End', metavar='', require=False, help="custom end time of clip")
+parser.add_argument('-a', '--automate', metavar='', required=False, action='store_true', help="automate script by having having file NAME_StartTime_EndTime.mp4")
+args = parser.parse_args()
 
 
 def blur(image):
@@ -14,17 +22,17 @@ def createResolutionClip():
 def createBackgroundClip(originalClip):
     clip = VideoFileClip(originalClip).fx(vfx.resize, width=3413)
     clip = clip.fl_image(blur)
-    clip = clip.set_position('center')
+    clip = clip.set_position("center")
     return clip
 #Sets up focus clip
 def createFocusClip(originalClip):
     clip = VideoFileClip(originalClip).fx(vfx.resize, width=1080)
-    clip = clip.set_position('center')
+    clip = clip.set_position("center")
     return clip
 
 
 if __name__ == '__main__':
-    clipFile = sys.argv[1]
+    clipFile = args.file
     clipName = clipFile[0:-4]
 
     resolutionClip = createResolutionClip()
